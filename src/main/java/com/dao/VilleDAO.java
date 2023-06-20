4970aba89f7bf073beadf032f47012830f2b5ae5
Jules-Steiner <97162125+Jules-Steiner@users.noreply.github.com>
2023-06-20 02:22:01 +0200

    end of twic

--- src/main/java/com/dao/VilleDAO.java
+++ src/main/java/com/dao/VilleDAO.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 import com.dto.Ville;
 
 public interface VilleDAO {
-
 	public ArrayList<Ville> findAllVilles();
 
+	public ArrayList<Ville> findVille(String codePostal);
+	
+	public boolean updateVille(Ville ville);
+	
+	public boolean createVille(Ville ville);
+	
+	public boolean deleteVille(String codeCommune);	
 }
